6ef668bc37523f585821876364851eb6eeddf4ea
Tim Jones <timjonez@protonmail.com>
2023-07-03 22:22:44 -0700

    generate project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    let todos = [
+        {
+            "name": "Do this",
+            "is_done": false
+        },
+        {
+            "name": "and that",
+            "is_done": false
+        }
+    ]
+    console.log('--------', todos)
+</script>
+
+<h1>Todos</h1>
+{#each todos as todo }
+    <div>
+        <label for="is_done">{todo.name}</label>
+        <input name="is_done" type="checkbox" bind:checked={todo.is_done} /> 
+    </div> 
+{/each}
